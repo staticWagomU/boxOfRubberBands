2f05477bee2a8f5999237f78560557138e2fb66a
staticWagomU <to6wa17@gmail.com>
2023-02-01 20:18:34 +0900

    説明追加！これでlighthouceで100点取れる！！！！！！！！！！

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = '輪ごむの空き箱';
-export const SITE_DESCRIPTION = '';
+export const SITE_DESCRIPTION = '輪ごむのブログです。';
