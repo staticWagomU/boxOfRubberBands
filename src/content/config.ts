ab39b15da708aa475796c457ada5638db91a94dc
staticWagomU <to6wa17@gmail.com>
2023-04-14 22:01:43 +0900

    v2移行完了のはず

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,18 @@
 import { defineCollection, z } from 'astro:content';
 
-const blog = defineCollection({
+const blogCollection = defineCollection({
 	// Type-check frontmatter using a schema
 	schema: z.object({
 		title: z.string(),
 		description: z.string(),
 		// Transform string to Date object
-		pubDate: z .string().or(z.date()).transform((val) => new Date(val)),
-		updatedDate: z .string().optional().transform((str) => (str ? new Date(str) : undefined)),
+		pubDate: z.date().transform((val) => new Date(val)),
+		updatedDate: z.date().transform((val) => new Date(val)).optional(),
 		heroImage: z.string().optional(),
+    tags: z.array(z.string()).optional(),
 	}),
 });
 
-export const collections = { blog };
+export const collections = <const>{
+  blog: blogCollection,
+};
