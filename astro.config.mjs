d94b736a4f6a99b26dbc895de9574ba38b1e0106
staticWagomU <to6wa17@gmail.com>
2023-05-19 21:45:42 +0900

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+import image from "@astrojs/image";
+
 // https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
   // site: 'https://example.com',
   site: 'https://staticWagomU.github.io/',
-  integrations: [mdx(), sitemap()]
+  integrations: [mdx(), sitemap(), image()]
 });
