61d0d8b193aca20f29d44a989fbadb31dffe6e76
staticWagomU <to6wa17@gmail.com>
2023-08-10 21:17:27 +0900

    ふぉーまっと！！！！！！！！！！！！！！！！

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,17 +1,20 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
 const blogCollection = defineCollection({
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.date().transform((val) => new Date(val)),
-		updatedDate: z.date().transform((val) => new Date(val)).optional(),
-		heroImage: z.string().optional(),
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    // Transform string to Date object
+    pubDate: z.date().transform((val) => new Date(val)),
+    updatedDate: z
+      .date()
+      .transform((val) => new Date(val))
+      .optional(),
+    heroImage: z.string().optional(),
     tags: z.array(z.string()).optional(),
     published: z.boolean(),
-	}),
+  }),
 });
 
 export const collections = <const>{
