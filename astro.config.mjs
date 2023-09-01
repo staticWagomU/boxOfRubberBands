736834cdec3c83067ac6554d1cf44377aa9a9f34
staticWagomU <to6wa17@gmail.com>
2023-09-01 22:33:30 +0900

    Removed: @astrojs/image Astro 3.0へのバージョンアップに伴い不要になったため

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-
-import image from "@astrojs/image";
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
   // site: 'https://example.com',
-  site: 'https://staticWagomU.github.io/',
-  integrations: [mdx(), sitemap(), image()]
+  site: "https://staticWagomU.github.io/",
+  integrations: [mdx(), sitemap()],
 });
