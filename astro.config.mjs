48682be4ed5cd848752e5b3657ed72e1bda40027
staticWagomU <to6wa17@gmail.com>
2023-09-06 23:03:30 +0900

    Add: add config for vanilla extract

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from "astro/config";
+import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
@@ -9,4 +10,7 @@ export default defineConfig({
   // site: 'https://example.com',
   site: "https://staticWagomU.github.io/",
   integrations: [mdx(), sitemap()],
+  vite: {
+    plugins: [vanillaExtractPlugin()],
+  },
 });
