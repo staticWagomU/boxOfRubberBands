75612bf5e811776e4afb0dd80464f8cb812fea44
staticWagomU <to6wa17@gmail.com>
2023-09-09 11:40:13 +0900

    Fix: rss feed

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,8 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-
-// https://astro.build/config
 export default defineConfig({
-  site: "https://wagomu.me/",
+  site: "https://wagomu.me",
   integrations: [mdx(), sitemap()],
   vite: {
     plugins: [vanillaExtractPlugin()],
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,19 +1,21 @@
 import rss from "@astrojs/rss";
-import { getContent } from "@astrojs/content";
+import { getCollection } from "astro:content";
 import { SITE_TITLE, SITE_DESCRIPTION } from "../config";
 import sanitizeHtml from "sanitize-html";
 import MarkdownIt from "markdown-it";
-
 const parser = new MarkdownIt();
 
 export async function GET(context) {
-  const blog = await getContent("blog");
+  const blog = await getCollection("blog");
+
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: context.site,
+    // items: await pagesGlobToRssItems(import.meta.glob("./blog/*.mdx")),
     items: blog.map((post) => ({
       link: `/blog/${post.slug}/`,
+      // 注: MDXファイルのコンポーネントやJSX式は処理されません。
       content: sanitizeHtml(parser.render(post.body)),
       ...post.data,
     })),
