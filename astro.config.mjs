3ef39158edf698dde825ebec9c651f7855879dc9
staticWagomU <to6wa17@gmail.com>
2024-08-05 16:13:05 +0900

    tailwindを導入したよ (#43)
    Close #39

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,24 @@
 import { defineConfig } from "astro/config";
-import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 import remarkBreaks from "remark-breaks";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
+
 // https://astro.build/config
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   // site: 'https://example.com',
   site: "https://wagomu.me",
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), tailwind()],
   vite: {
-    plugins: [vanillaExtractPlugin()],
+    plugins: []
   },
   markdown: {
     remarkPlugins: [remarkBreaks],
     shikiConfig: {
-      theme: "github-light",
-    },
-  },
+      theme: "github-light"
+    }
+  }
 });
