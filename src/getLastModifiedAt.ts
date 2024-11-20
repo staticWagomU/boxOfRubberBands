12a69fc3006d48f059b0ffec780f3f9c8f6e503d
staticWagomU <to6wa17@gmail.com>
2024-11-20 12:56:38 +0900

    feat!: はじめからやりなおすよ！！！

--- src/getLastModifiedAt.ts
+++
@@ -1,6 +0,0 @@
-import { execSync } from "child_process";
-
-export function getLastModifiedAt(filePath: string): string {
-  const result = execSync(`git log -1 --format=%cs ${filePath}`);
-  return result.toString().trim();
-}
