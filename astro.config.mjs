51fc469300dd60d3773662874b163b2ca9565b8a
staticWagomU <to6wa17@gmail.com>
2025-01-28 18:14:24 +0900

    feat: remark, rehypeプラグインを整理

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,9 @@ import { defineConfig } from "astro/config";
 import remarkBreaks from "remark-breaks";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import codeBlockPlugin from "./tools/remark-code-quote";
-import wrapH2WithSection from "./tools/rehype-wrap-h2-with-section";
 import astroLastModifiedAt from "./tools/remark-astro-last-modified-at";
-import remarkBudoux from "./tools/remark-budoux";
+import codeBlockPlugin from "./tools/remark-code-quote";
+import rehypeHeadingSpan from "./tools/rehype-heading-span";
 import rehypeLineNumbers from "./tools/rehype-line-numbers";
 
 import compressor from "astro-compressor";
@@ -39,8 +38,8 @@ export default defineConfig({
 			footnoteLabelTagName: "hr",
 			footnoteLabel: " ",
 		},
-		remarkPlugins: [remarkBreaks, astroLastModifiedAt, remarkBudoux, codeBlockPlugin],
-		rehypePlugins: [wrapH2WithSection, rehypeLineNumbers],
+		remarkPlugins: [remarkBreaks, astroLastModifiedAt, codeBlockPlugin],
+		rehypePlugins: [rehypeLineNumbers, rehypeHeadingSpan],
 		shikiConfig: {
 			defaultColor: false,
 			themes: {
---
+++ tools/rehype-heading-span/index.js
@@ -0,0 +1,16 @@
+import { visit } from "unist-util-visit";
+
+export default function rehypeHeadingSpan() {
+	return function transformer(tree) {
+		visit(tree, 'element', (node) => {
+			if ([ 'h1', 'h2', 'h3', 'h4', 'h5', 'h6' ].includes(node.tagName)) {
+				node.children = [{
+					type: 'element',
+					tagName: 'span',
+					properties: {},
+					children: node.children,
+				}];
+			}
+		});
+	};
+};
